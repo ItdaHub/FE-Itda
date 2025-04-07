300f851bdd0f1565e9a7f21a375c29bf349b48cc
Taetae1123 <xodus2079@naver.com>
2025-04-08 01:05:56 +0900

    Fix: 글쓰기 수정

--- fe-itda/src/pages/newwrite/index.tsx
+++ fe-itda/src/pages/newwrite/index.tsx
@@ -1,14 +1,22 @@
 import NewWrite from "@/components/NewWrite";
 import { useRouter } from "next/router";
 
-const newwrite = () => {
+const NewWritePage = () => {
   const router = useRouter();
   const { type, title, genre } = router.query;
+
+  // 아직 query 안 들어왔을 때
+  if (!type || Array.isArray(type)) return null;
+
   return (
     <div>
-      <NewWrite type={type} titles={title} genres={genre} />
+      <NewWrite
+        type={type === "relay" ? "relay" : "new"}
+        titles={typeof title === "string" ? title : ""}
+        genres={typeof genre === "string" ? genre : ""}
+      />
     </div>
   );
 };
 
-export default newwrite;
+export default NewWritePage;
