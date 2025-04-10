2821f258d5540b5e60fa202e062bc5550ff35ae9
Taetae1123 <xodus2079@naver.com>
2025-04-10 19:44:07 +0900

    Fix: 에피소드 정렬+챕터 정보 수정

--- fe-itda/src/features/Episode/index.tsx
+++ fe-itda/src/features/Episode/index.tsx
@@ -1,13 +1,14 @@
 import clsx from "clsx";
+import dayjs from "dayjs";
 import { EpisodeStyled } from "./styled";
 
-const Episode = ({ item, index }: { item: any; index: number }) => {
+const Episode = ({ item }: { item: any }) => {
   return (
     <EpisodeStyled className={clsx("episode-wrap")}>
-      <div color="episode-num">{index}화</div>
+      <div color="episode-num">{item.chapter_number}화</div>
       <div className="episode-info">
-        <div className="episode-comment-num">댓글 {item.commentNum}</div>
-        <div>{item.createDate}</div>
+        <div className="episode-comment-num">댓글 {item.comments.length}</div>
+        <div>{dayjs(item.created_at).format("YYYY-MM-DD HH:mm:ss")}</div>
       </div>
     </EpisodeStyled>
   );
