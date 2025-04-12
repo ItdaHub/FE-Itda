db9443ee73e3bffa141b2a1f6d11d931dbd3bca0
Taetae1123 <xodus2079@naver.com>
2025-04-13 01:41:32 +0900

    Fix: 처음에 floatbutton안뜨는 오류 수정(main파일이 2개였음)

--- fe-itda/src/features/MainPageManager/MainPage/index.tsx
+++ fe-itda/src/features/MainPageManager/MainPage/index.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 import { MainPageStyled } from "./styled";
+import CreateIcon from "@mui/icons-material/Create";
+import { FloatButton } from "antd";
+import { useRouter } from "next/router";
+
 import Category from "@/features/Category";
 import clsx from "clsx";
 import api from "@/utill/api";
@@ -13,6 +17,20 @@ const MainPage = () => {
 
   // 장르
   const [genre, setGenre] = useState<string>("all");
+
+  const [isMobile, setIsMobile] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // 처음 로드 시
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   // 렌더링시 카테고리 불러오기(한번)
   useEffect(() => {
@@ -50,6 +68,28 @@ const MainPage = () => {
         setGenre={setGenre}
         categories={categories}
       />
+      {isMobile ? (
+        <FloatButton
+          icon={
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+                width: "100%",
+              }}
+            >
+              <CreateIcon style={{ fontSize: 20 }} />
+            </div>
+          }
+          type="primary"
+          style={{ right: 20, bottom: 80 }}
+          onClick={() => router.push(`/newwrite?type="new"`)}
+        />
+      ) : (
+        <FloatButton.BackTop visibilityHeight={0} style={{ bottom: 80 }} />
+      )}
     </MainPageStyled>
   );
 };
