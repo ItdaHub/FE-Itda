cb71a5621988ad51a0810c70f80b518e28e70f7b
Taetae1123 <xodus2079@naver.com>
2025-04-30 14:45:00 +0900

    Fix: banner css 수정

--- fe-itda/src/features/Banner/styled.ts
+++ fe-itda/src/features/Banner/styled.ts
@@ -16,8 +16,8 @@ export const BannerStyled = styled.div`
 
     .banner-img {
       width: 100%;
-      height: 382px;
-      object-fit: cover;
+      height: 100%;
+      object-fit: contain;
       overflow: hidden;
     }
   }
